f192859997b5d6c34d71622fe4c034f2ef322187
leojunioyuri <leojunioyuri@hotmail.com>
2023-01-03 15:03:29 -0300

    feat: UseState no componente de input

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,26 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 import InputField from "./components/InputField/InputField";
+import { Todo } from "./model";
 
 const App: React.FC = () => {
+
+  const [todo, setTodo] = useState<string>("");
+  const [todos, setTodos] = useState<Todo[]>([])
+
+  const handleAddTodo = (e: React.FormEvent) => {
+    e.preventDefault();
+    if (todo) {
+      setTodos([ ...todos, {id: Date.now(), todo, isDone: false} ])
+      setTodo("");
+    }
+  };
+  console.log(todos);
+
   return (
     <div className="App">
       <span className="heading">Taskify</span>
-      <InputField></InputField>
-      <p>Hello World</p>
+      <InputField todo={todo} setTodo={setTodo} handleAddTodo={handleAddTodo}></InputField>
     </div>
   );
 }
--- src/components/InputField/InputField.tsx
+++ src/components/InputField/InputField.tsx
@@ -1,10 +1,22 @@
 import React from "react";
-import "./InputField.css"
+import "./InputField.css";
 
-const InputField = () => {
+interface Props {
+  todo: string;
+  setTodo: React.Dispatch<React.SetStateAction<string>>;
+  handleAddTodo: (e: React.FormEvent) => void;
+}
+
+const InputField: React.FC<Props> = ({ todo, setTodo, handleAddTodo }) => {
   return (
-    <form className="input">
-      <input type="input" placeholder="Enter a task" className="inputBox" />
+    <form className="input" onSubmit={handleAddTodo}>
+      <input
+        type="input"
+        value={todo}
+        onChange={(e) => setTodo(e.target.value)}
+        placeholder="Enter a task"
+        className="inputBox"
+      />
       <button className="inputSubmitButton" type="submit">
         Go
       </button>
